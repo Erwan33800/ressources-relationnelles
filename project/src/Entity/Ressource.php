e7a734124f2087682e00a6d3bb36eeaaed2337b6
Erwan LE PIERRES <55241151+Erwan33800@users.noreply.github.com>
2024-01-03 13:14:45 +0100

    feat(entity): create all entities with simple id and migrate db

---
+++ project/src/Entity/Ressource.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RessourceRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: RessourceRepository::class)]
+#[UniqueEntity(
+    fields: ['title'],
+    message: 'Une autre ressource possède déjà ce titre, merci de le modifier.'
+)]
+#[ORM\HasLifecycleCallbacks]
+class Ressource
+{
+    const STATES = [
+        'STATE_DRAFT',
+        'STATE_PUBLISHED',
+    ];
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[Assert\NotBlank()]
+    private string $title;
+
+    #[ORM\Column(type: 'string')]
+    #[Assert\NotBlank()]
+    private string $address;
+
+    #[ORM\Column(type: 'string')]
+    #[Assert\NotBlank()]
+    private string $description;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotNull()]
+    private string $state = self::STATES[0];
+
+    #[ORM\Column(type: 'integer')]
+    private int $nbLike = 0;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $eventDate;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $updatedAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTimeImmutable();
+    }
+
+    #[ORM\PreUpdate]
+    public function preUpdate(): void
+    {
+        $this->updatedAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getEventDate(): \DateTimeImmutable
+    {
+        return $this->eventDate;
+    }
+
+    public function setEventDate(\DateTimeImmutable $eventDate): self
+    {
+        $this->eventDate = $eventDate;
+
+        return $this;
+    }
+
+    public function getNbLike(): int
+    {
+        return $this->nbLike;
+    }
+
+    public function setNbLike(int $nbLike): self
+    {
+        $this->nbLike = $nbLike;
+
+        return $this;
+    }
+    
+    public function __toString(): string
+    {
+        return $this->getTitle();
+    }
+}
